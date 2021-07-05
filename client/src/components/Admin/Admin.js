68e604fa55f06e4e982706170d2bdade0a8b4452
ksbin1025 <ksbin1025@naver.com>
2021-07-05 16:18:44 +0900

    admin text-dark

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -11,16 +11,16 @@ const Admin = () => {
         <div className={`d-md-flex align-items-md-start ${styles.box}`}>
             <nav className={`col-md-3 col-lg-2 nav flex-md-column flex-row ${styles.navbar}`} id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 <li className="nav-item">
-                    <Link to={`${match.url}/movie`} className="nav-link text-center text-dark active">영화 관리</Link>
+                    <Link to={`${match.url}/movie`} className="nav-link text-dark text-center active">영화 관리</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to={`${match.url}/theater`} className="nav-link text-center text-dark">상영관 관리</Link>
+                    <Link to={`${match.url}/theater`} className="nav-link text-dark text-center">상영관 관리</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to={`${match.url}/cinema`} className="nav-link text-center text-dark">영화관 관리</Link>
+                    <Link to={`${match.url}/cinema`} className="nav-link text-dark text-center">영화관 관리</Link>
                 </li>
             </nav>
-            <div className="col-md-9 col-lg-10 tab-content" id="v-pills-tabContent">
+            <div className="tab-content container col-md-9 col-lg-10 mt-3" id="v-pills-tabContent">
                 <Switch>
                     <Route path={`${match.path}/movie`}><MovieEdit /></Route>
                     <Route path={`${match.path}/theater`}><TheaterEdit /></Route>
