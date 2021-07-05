9fdebb842bf8130fe44dbc9b62e5e402e476289f
Jiwon Yoon <jiwon5393@naver.com>
2021-07-05 11:41:20 +0900

    boxoffice-margin

--- client/src/components/BoxOffice/BoxOffice.js
+++ client/src/components/BoxOffice/BoxOffice.js
@@ -23,7 +23,7 @@ const BoxOffice = () => {
 
     return (
         <div className="container">
-            <h2 className="fw-bold text-white text-center my-5">Box Office</h2>
+            <h2 className="fw-bold text-white text-center mt-5">Box Office</h2>
             <div id="multi-carousel" className={`d-flex carousel slide align-items-center ${styles.customHeight}`} data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
