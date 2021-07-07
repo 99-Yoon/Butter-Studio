d1206e75ed94431f8db211312be086fcfda01cc2
ksbin1025 <ksbin1025@naver.com>
2021-07-07 16:20:20 +0900

    import Pagination & MovieTable

--- client/src/components/Admin/MovieEdit.js
+++ client/src/components/Admin/MovieEdit.js
@@ -1,11 +1,19 @@
 import Search from "../Search";
+import MovieTable from "../MovieTable";
+import Pagination from "../Pagination";
 
 const MovieEdit = () => {
     return (
-        <div className="d-flex justify-content-around">
-            <button type="button" className="btn btn-dark" style={{ width: "5em" }}>등록</button>
-            <Search type="admin" />
-        </div>
+        <>
+            <div className="d-flex justify-content-end mb-3">
+                <Search type="admin" />
+            </div>
+            <MovieTable />
+            <div className="d-flex align-items-center">
+                <Pagination />
+                <button type="button" className="btn btn-dark" style={{ width: "5em" }}>등록</button>
+            </div>
+        </>
     )
 }
 export default MovieEdit
