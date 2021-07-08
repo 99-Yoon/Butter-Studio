3411474dd052f4a7389bb5ece86262559f2e10a2
ksbin1025 <ksbin1025@naver.com>
2021-07-08 14:51:49 +0900

    Search admin style 변경

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -5,7 +5,7 @@ const Search = ({ type }) => {
     return (
         <div className="d-flex">
             <input className="form-control" type="text" id={type === "home" ? styles.searchWhite : styles.search} />
-            <i className={`bi bi-search align-self-center ${type === "home" ? "text-white" : "mx-1"} ${styles.icon}`} style={{ fontSize: "1.3rem" }}></i>
+            <i className={`bi bi-search align-self-center ${type === "home" ? "text-white" : "mx-2"} ${styles.icon}`} style={{ fontSize: "1.3rem" }}></i>
         </div>
     )
 }
