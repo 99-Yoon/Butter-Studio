d76ed4641359990064935a3b54d7b9aba9a90d9b
ksbin1025 <ksbin1025@naver.com>
2021-07-23 22:49:44 +0900

    cinemaApi

---
+++ client/src/apis/cinema.api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import baseUrl from "../utils/baseUrl.js";
+
+const getInfo = async () => {
+    const { data } = await axios.get(`${baseUrl}/api/cinema`)
+    return data
+}
+
+const edit = async (cinemaInfo) => {
+    const { data } = await axios.put(`${baseUrl}/api/cinema`, cinemaInfo)
+    return data
+}
+
+const cinemaApi = {
+    getInfo,
+    edit
+}
+
+export default cinemaApi
