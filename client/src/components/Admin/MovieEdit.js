80093bc257f978978ce43c31ca3f8627c32a6bd7
ksbin1025 <ksbin1025@naver.com>
2021-07-23 14:21:02 +0900

    import styles 해제

--- client/src/components/Admin/MovieEdit.js
+++ client/src/components/Admin/MovieEdit.js
@@ -4,7 +4,6 @@ import MovieTable from "../MovieTable";
 import Pagination from "../Pagination";
 import movieApi from "../../apis/movie.api.js";
 import catchErrors from "../../utils/catchErrors.js";
-import styles from "./admin.module.scss";
 
 const MovieEdit = () => {
     const [search, setSearch] = useState({ type: "admin", keyword: "" })
