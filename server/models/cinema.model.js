a276f40fd563faf818e3b1054720ef3ab09edc5c
ksbin1025 <ksbin1025@naver.com>
2021-07-23 22:53:35 +0900

    cinema Model

---
+++ server/models/cinema.model.js
@@ -0,0 +1,36 @@
+import Sequelize from "sequelize";
+
+const { DataTypes } = Sequelize;
+
+const CinemaModel = (sequelize) => {
+    const Cinema = sequelize.define(
+        "cinema",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            cinemaName: {
+                type: DataTypes.STRING,
+            },
+            transportation: {
+                type: DataTypes.TEXT
+            },
+            parking: {
+                type: DataTypes.TEXT
+            },
+            address: {
+                type: DataTypes.STRING
+            }
+        },
+        {
+            timestamps: true,
+            freezeTableName: true,
+            tableName: "cinemas"
+        }
+    );
+    return Cinema;
+};
+
+export default CinemaModel;
