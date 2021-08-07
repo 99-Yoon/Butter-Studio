7b1891102d3764db36b03c2b6c8907515aacfd05
ksbin1025 <ksbin1025@naver.com>
2021-08-07 12:55:07 +0900

    공백

--- client/src/components/Admin/TimeTableEdit.js
+++ client/src/components/Admin/TimeTableEdit.js
@@ -2,6 +2,7 @@ import TimeTableEditForm from "./TimeTableEditForm";
 import TimeTable from "./TimeTable";
 
 const TimeTableEdit = () => {
+
     return (
         <>
             <h2 className="border-bottom border-2 text-center pb-2 me-2">현재 상영시간표 정보</h2>
