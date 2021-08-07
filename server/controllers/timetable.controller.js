931380b0a0b9d3a4a8baf8abdd1d911770299063
ksbin1025 <ksbin1025@naver.com>
2021-08-07 17:45:20 +0900

    timetableCtrl 수정 완료

--- server/controllers/timetable.controller.js
+++ server/controllers/timetable.controller.js
@@ -1,4 +1,4 @@
-import { TimeTable, Theater } from "../db/index.js";
+import { TimeTable, Theater, TheaterType } from "../db/index.js";
 import moment from 'moment';
 import sequelize from 'sequelize'
 const { Op } = sequelize
@@ -8,21 +8,18 @@ const getAll = async (req, res) => {
         const { when } = req.query
         const selectDate = new Date(when)
         const theaterArr = []
-        // const timeTableArr = []
         const findAll = await TimeTable.findAll({ where: { date: selectDate }, attributes: { exclude: ['createdAt', 'updatedAt'] }, order: [["theater", "ASC"], ["start_time", "ASC"]] })
         findAll.forEach(element => {
             if (!theaterArr.includes(element.theater)) theaterArr.push(element.theater)
         })
-        const findTheater = await Theater.findAll({ where: { id: theaterArr }, attributes: { exclude: ['createdAt', 'updatedAt'] }, order: [['theaterName']] })
+        const findTheater = await Theater.findAll({ where: { id: theaterArr }, attributes: { exclude: ['createdAt', 'updatedAt'] }, include: [TheaterType], order: [['theaterName']] })
         findTheater.forEach(el => {
             const arr = findAll.filter(timetable => {
                 if (el.id === timetable.theater) return timetable.dataValues
             })
             el.dataValues.timetable = arr
-            // timeTableArr.push({ id: el.id, info: arr })
         })
         return res.json(findTheater)
-        // return res.json({findTheater, timeTableArr})
     } catch (error) {
         return res.status(500).send(error.message || "상영시간표 정보 가져오는 중 에러 발생")
     }
@@ -62,7 +59,7 @@ const submit = async (req, res) => {
             await Promise.all(
                 theater.map(async (theater) => {
                     let partTime = ""
-                    if ('06:00' <= theater.start < '10:00') partTime = "morning"
+                    if ('06:00' <= theater.start && theater.start < '10:00') partTime = "morning"
                     else if ('00:00' <= theater.start < '06:00') partTime = "night"
                     else partTime = "day"
                     await TimeTable.create({ theater: theater.theater, movieId, title, release_date, date: curDate, start_time: getTime(theater.start), end_time: getTime(theater.start, runtime), partTime: partTime, week: (day === 0 || day === 6) ? "weekend" : "weekdays" })
@@ -85,7 +82,7 @@ const getTime = (string, runtime = 0) => {
 const remove = async (req, res) => {
     try {
         const { timeId } = req.params
-        const delNum = await TimeTable.destroy({ where: {} })
+        const delNum = await TimeTable.destroy({ where: { id: timeId } })
         if (delNum) res.json(delNum)
         else throw new Error("해당 정보를 서버에서 삭제하는데 실패했습니다.")
     } catch (error) {
