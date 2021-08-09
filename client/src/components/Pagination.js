89dc08ee91cd699ac93f01bb3e5bada2f56edcaa
ksbin1025 <ksbin1025@naver.com>
2021-08-09 13:13:01 +0900

    Pagination 주석 삭제

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -43,26 +43,6 @@ const Pagination = ({ totalPages, activePage, prevPage, nextPage, paginate }) =>
             />
         </PaginationBoot>
     )
-
-    // return (
-    //     <nav className="col-12 col-md-4 offset-md-4 my-2" aria-label="Page navigation">
-    //         <ul className="pagination justify-content-center mb-0">
-    //             <li className="page-item">
-    //                 <a className="page-link" href="#" aria-label="Previous">
-    //                     <span aria-hidden="true">&laquo;</span>
-    //                 </a>
-    //             </li>
-    //             <li className="page-item"><a className="page-link" href="#">1</a></li>
-    //             <li className="page-item"><a className="page-link" href="#">2</a></li>
-    //             <li className="page-item"><a className="page-link" href="#">3</a></li>
-    //             <li className="page-item">
-    //                 <a className="page-link" href="#" aria-label="Next">
-    //                     <span aria-hidden="true">&raquo;</span>
-    //                 </a>
-    //             </li>
-    //         </ul>
-    //     </nav>
-    // )
 }
 
 export default Pagination
